139409758158481bbc1e2f4554d08902810bb20a
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-05-25 12:49:30 -0400

    listing comments

---
+++ src/components/CommentForm.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const CommentForm = (props) => {
+    const { id } = useParams()
+    const [ description, setDescription] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/movies/${id}/comment`, {
+            description
+        }, {
+            headers: {
+                AUthorization: localStorage.getItem('userId')
+            }
+        })
+        .then((response) => {
+            setDescription('')
+            props.setShouldReload(true)
+        })
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="description"></label>
+            <input id="comment-input" placeholder="Your comment here" value={description} onChange={ (e)=> {setDescription(e.target.value)}}></input>
+            <input id="comment-submit" type="submit" value="submit" />
+        </form>
+    )
+}
+
+export default CommentForm
--- src/pages/SingleMovie.js
+++ src/pages/SingleMovie.js
@@ -1,20 +1,26 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import CommentForm from '../components/CommentForm'
 
 
 const SingleMovie = () => {
     const { id } = useParams()
     const [movieInfo, setMovieInfo] = useState({})
+    const [shouldReload, setShouldReload] = useState(true)
 
     const fetchSingleMovie = () => {
+        if(!shouldReload) { return }
+
         axios.get(`${process.env.REACT_APP_BACKEND_URL}/movies/${id}`)
         .then((response) => {
             setMovieInfo(response.data)
+            setShouldReload(false)
         })
     }
 
     useEffect(fetchSingleMovie, [])
+    useEffect(fetchSingleMovie, [shouldReload])
 
     const saveMovies = (e) => {
         axios.post(`${process.env.REACT_APP_BACKEND_URL}/movies/${id}`,
@@ -28,23 +34,43 @@ const SingleMovie = () => {
     }
 
     return(
-        <div className="singleMovieContainer">
-        <>
-        {
-            movieInfo.movie ?
+        <div className="singleMovie-container" >
+            <div className="movie-info">
             <>
-            <h1>{movieInfo.movie.title}</h1>
-            <img src={movieInfo.movie.image}></img>
-            <h3>{movieInfo.movie.plot}</h3>
-            <iframe width="560" height="315" src={movieInfo.movie.videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <button onClick={saveMovies}>Save to my list</button>
+            {
+                movieInfo.movie ?
+                <>
+                <h1>{movieInfo.movie.title}</h1>
+                <img src={movieInfo.movie.image}></img>
+                <h3>{movieInfo.movie.plot}</h3>
+                <iframe width="560" height="315" src={movieInfo.movie.videoUrl} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <button onClick={saveMovies}>Save to my list</button>
+                </>
+                :
+                
+                <p>
+            Loading...</p>
+            }
             </>
-            :
-            
-            <p>
-        Loading...</p>
-        }
-        </>
+            </div>
+            <div className="commentForm-container">
+                <>
+                <CommentForm setShouldReload={setShouldReload}/>
+                </>
+                <div className="comment-container">
+                    {
+                        movieInfo.comments &&
+                        movieInfo.comments.map((comment) => {
+                            return <div key={comment.id}>
+                                        <div>
+                                        <p>{comment.user.name}{' | '}{comment.description}</p>
+                                        <p>{comment.createdAt.substr(0,10)}</p>
+                                        </div>
+                                   </div>
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
